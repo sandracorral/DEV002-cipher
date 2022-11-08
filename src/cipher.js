5eee004b195e37911cffb96151ab578ee49b92a1
Sandra <sandy.corralt@gmail.com>
2022-11-08 17:05:51 -0600

    comentario prueba avance codigo

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,43 @@
+
 const cipher = {
-  // ...
+  encode: (offset, string) {
+    if (!string)
+      return "";
+    const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    offset = (offset % 26 + 26) % 26;
+    return string.replace(/[A-Z]/ig, c => letras[(letras.indexOf(c) + offset) % 26]);
+  },
+
+  decode: (offset, string) {
+    if (!string)
+      return "";
+    const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    offset = (offset % 26 - 26) % 26;
+    return string.replace(/[A-Z]/ig, c => letras[(letras.indexOf(c) - offset) % 26]);
+  }
 };
 
 export default cipher;
+
+
+
+/* Funciones cifrado*/
+/*
+ function cifrar(texto, desplazamiento) {
+  if (!texto)
+      return "";
+  const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  desplazamiento = (desplazamiento % 26 + 26) % 26;
+  return texto.replace(/[A-Z]/ig, c => letras[(letras.indexOf(c) + desplazamiento) % 26]);
+}
+
+Funciones descifrado
+
+function descifrar(texto, desplazamiento) {
+  if (!texto)
+      return "";
+  const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  desplazamiento = (desplazamiento % 26 - 26) % 26;
+  return texto.replace(/[A-Z]/ig, c => letras[(letras.indexOf(c) - desplazamiento) % 26]);
+}
+*/
--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+console.log("hola mundo");
+//window.addEventListener("load", inicio, true);
+const CajaTexto = document.getElementById("CajaDeTexto");
+
+//function inicio() {
+
+    /*Para hacer todo el texto en mayúsculas*/
+    CajaTexto.addEventListener("keyup", function () {
+        CajaTexto.value = CajaTexto.value.toUpperCase();
+    });
+    
+    /*Cifrar*/
+    document.getElementById("cifrar").addEventListener("click", function () {
+        let texto = document.getElementById("CajaDeTexto").value;
+        let desplazamiento = document.getElementById("desplazamiento").value;
+        document.getElementById("MensajeCifrado").value = cipher.encode(desplazamiento, texto);
+    }, true);
+
+    /*Descifrar*/
+    document.getElementById("descifrar").addEventListener("click", function () {
+        let texto = document.getElementById("CajaDeTexto").value;
+        let desplazamiento = document.getElementById("desplazamiento").value;
+        document.getElementById("MensajeCifrado").value = cipher.decode(desplazamiento, texto);
+    }, true);
+//}
+
+
+    /*Funciones cifrado*/
+/*
+    function cifrar(texto, desplazamiento) {
+        if (!texto)
+            return "";
+        const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        desplazamiento = (desplazamiento % 26 + 26) % 26;
+        return texto.replace(/[A-Z]/ig, c => letras[(letras.indexOf(c) + desplazamiento) % 26]);
+    }
+
+    function descifrar(texto, desplazamiento) {
+        if (!texto)
+            return "";
+        const theAlfa = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        desplazamiento = (desplazamiento % 26 - 26) % 26;
+        return texto.replace(/[A-Z]/ig, c => theAlfa[(theAlfa.indexOf(c) - desplazamiento) % 26]);
+    } */
